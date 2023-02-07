c63c98fde152dc35de0a8ce0dff6f12c197ef075
Habeeb10 <79145763+Habeeb10@users.noreply.github.com>
2023-02-07 02:41:43 +0100

    chore:installed app packages

---
+++ src/screens/auth/components/FirebaseRegistration.tsx
@@ -0,0 +1,37 @@
+import {firebase} from '../../../firebase/config';
+import '@firebase/firestore';
+import 'firebase/auth';
+import '@react-native-firebase/auth';
+type RegistrationType = {
+  email: string;
+  password: string;
+  // phoneNumber: number;
+  navigation: any;
+};
+export const Registration = async ({
+  navigation,
+  email,
+  password,
+}: // phoneNumber,
+RegistrationType) => {
+  const response = await firebase
+    .auth()
+    .createUserWithEmailAndPassword(email, password);
+  const uid = response.user.uid;
+  const data = {
+    id: uid,
+    email: '',
+    password: '',
+  };
+  const usersRef = firebase.firestore().collection('users');
+  usersRef
+    .doc(uid)
+    .set(data)
+    .then(() => {
+      navigation.navigate('signin', {user: data});
+    })
+    .catch((error: any) => {
+      alert(error);
+    });
+  console.log(email, password);
+};
