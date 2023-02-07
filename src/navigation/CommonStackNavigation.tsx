e9bf7aad0ef744fb6bf69b9550b2fb188d04db31
Habeeb10 <79145763+Habeeb10@users.noreply.github.com>
2023-02-07 02:44:56 +0100

    refactor:navigation

--- src/navigation/CommonStackNavigation.tsx
+++ src/navigation/CommonStackNavigation.tsx
@@ -3,6 +3,8 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import SignUp from '../screens/auth/signup/SignUp';
 import SignIn from '../screens/auth/signin/SignIn';
+import NewsListing from '../screens/newsList/NewsList';
+import NewsDetails from '../screens/newsDetails/NewsDetails';
 
 const Stack = createStackNavigator();
 
@@ -12,6 +14,8 @@ function Navigator() {
       <Stack.Navigator headerMode="none">
         <Stack.Screen name="signup" component={SignUp} />
         <Stack.Screen name="signin" component={SignIn} />
+        <Stack.Screen name="newsListing" component={NewsListing} />
+        <Stack.Screen name="newsDetails" component={NewsDetails} />
       </Stack.Navigator>
     </NavigationContainer>
   );
